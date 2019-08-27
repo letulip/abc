7a9b00d803d14a28058b72650befafe7304d2599
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2019-08-27 14:52:37 +0300

    feat: add gitignore, images, modify sponki layout, index.js, styles

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const folder = document.querySelector(`.folder`);
 const folderHeadings = folder.querySelectorAll(`.folder__heading`);
 
-const folderHeadingOnClick = (evt, heading) => {
+const folderHeadingOnClick = (evt) => {
   const files = folder.querySelectorAll(`.folder__file`);
   files.forEach((file) => {
     const dropdownIcon = file.querySelector(`.folder__dropdownIcon--dropped`);
@@ -16,11 +16,13 @@ const folderHeadingOnClick = (evt, heading) => {
   });
 
   const targetDropdown = evt.target.querySelector(`.folder__dropdownIcon`);
-  targetDropdown.classList.toggle(`folder__dropdownIcon--dropped`);
+  targetDropdown.classList.add(`folder__dropdownIcon--dropped`);
+  const dropdown = evt.target.nextElementSibling;
+  dropdown.classList.add(`dropdown--visible`);
 }
 
 folderHeadings.forEach((heading) => {
-  heading.addEventListener(`click`, (evt, heading) => {
-    folderHeadingOnClick(evt, heading);
+  heading.addEventListener(`click`, (evt) => {
+    folderHeadingOnClick(evt);
   });
 });
