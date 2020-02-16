676bab0fbb1fcfd8ed473f409511ec415ac1c11a
letulip <ivladimirskiy@ya.ru>
2020-02-16 18:40:16 +0300

    correct: tabs.js with subtabs initial functional

--- src/blocks/tabs/tabs.js
+++ src/blocks/tabs/tabs.js
@@ -2,28 +2,61 @@ const tabs = document.querySelector(`.tabs`);
 const tabsFirst = tabs.querySelectorAll(`.tabs__tab--first`);
 const tabsContentFirst = tabs.querySelectorAll(`.tabs__tabContent--first`);
 
-const setActiveTab = (tabs, index) => {
+const searchAllTabs = (content, contentIndex) => {
+  const tabsSecond = content.querySelectorAll(`.tabs__tab--second`);
+  const tabsContentSecond = tabsContentFirst[contentIndex].querySelectorAll(`.tabs__tabContent--second`);
+  tabsSecond.forEach((tab, index) => {
+    tab.addEventListener(`click`, () => {
+      console.log(`${contentIndex} tab ${index} second level click`);
+      tabSecondEventListner(tabsSecond, index, tabsContentSecond);
+    });
+  
+  });
+};
+
+const setActiveTabFirst = (tabs, index) => {
   tabs.forEach((tab) => {
     tab.classList.remove(`tabs__tab--active`);
   })
   tabs[index].classList.add(`tabs__tab--active`);
 };
 
-const setActiveTabContent = (index) => {
-  tabsContentFirst.forEach((tabContent) => {
-    tabContent.classList.remove(`tabs__tabContent--active`);
+const setActiveTabSecond = (tabs, index) => {
+  tabs.forEach((tab) => {
+    tab.classList.remove(`tabs__tab--second-active`);
   })
-  tabsContentFirst[index].classList.add(`tabs__tabContent--active`);
+  tabs[index].classList.add(`tabs__tab--second-active`);
 };
 
-const tabFirstEventListner = (tabs, index) => {
-  setActiveTab(tabs, index);
-  setActiveTabContent(index);
+const setActiveTabContent = (tabsContent, index) => {
+  console.log(tabsContent);
+  
+  tabsContent.forEach((tabContent) => {
+    tabContent.classList.remove(`tabs__tabContent--active`);
+    // tabContent.classList.remove(`tabs__tabContent--second-active`);
+  })
+  tabsContent[index].classList.add(`tabs__tabContent--active`);
+  // tabsContent[index].classList.add(`tabs__tabContent--second-active`);
+};
+
+const tabFirstEventListner = (tabs, index, content) => {
+  setActiveTabFirst(tabs, index);
+  setActiveTabContent(content, index);
+};
+
+const tabSecondEventListner = (tabs, index, content) => {
+  setActiveTabSecond(tabs, index);
+  setActiveTabContent(content, index);
 };
 
 tabsFirst.forEach((tab, index) => {
   tab.addEventListener(`click`, () => {
     console.log(`tab ${index} first level click`);
-    tabFirstEventListner(tabsFirst, index);
+    tabFirstEventListner(tabsFirst, index, tabsContentFirst);
   });
+
+});
+
+tabsContentFirst.forEach((content, index) => {
+  searchAllTabs(content, index);
 });
