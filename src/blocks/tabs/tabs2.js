959e138493712409de8468108d96bdcc133deae9
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2020-04-24 15:17:24 +0300

    feat: add setActiveTabInput function tabs2.js

--- src/blocks/tabs/tabs2.js
+++ src/blocks/tabs/tabs2.js
@@ -1,14 +1,9 @@
-const tabs = document.querySelectorAll(`.tabs__tab--second`);
+const tabsSecond = document.querySelector(`.tabs__second`);
+const tabs = tabsSecond.querySelectorAll(`.tabs__tab--second`);
 const url = new URL(window.location.href);
 const urlPathname = url.pathname.split('/');
 
 let urlPageParam = ``;
-
-const assignUrl = (tab, index) => {
-  tabOnClick(tab, index);
-  url.searchParams.set(urlPathname[2], index);
-  window.location.assign(url.href);
-};
 
 const removePrevActive = () => {
   tabs.forEach((tab) => {
@@ -18,18 +13,21 @@ const removePrevActive = () => {
   })
 };
 
+const setActiveTabInput = (index) => {
+  const tabInput = tabsSecond.querySelector(`#tab${index}`);
+  tabInput.checked = true;
+};
+
 const tabOnClick = (tab) => {
   removePrevActive();
   tab.classList.add(`tabs__tab--second-active`);
-  
 };
 
 tabs.forEach((tab, index) => {
   tab.addEventListener(`click`, (evt) => {
-    // assignUrl(evt.target, index);
     urlPageParam = `${urlPathname[2]}=${index}`
     generateUrlParam(urlPageParam);
-    tabOnClick(evt.target, index);
+    tabOnClick(evt.target);
   });
 });
 
@@ -46,9 +44,11 @@ if (document.referrer.split('?').length === 1 ||
   urlPageParam = `${urlPathname[2]}=0`
   generateUrlParam(urlPageParam);
   tabOnClick(tabs[0]);
+  setActiveTabInput(0);
 } else {
   const params = document.referrer.split('?');
   generateUrlParam(params[1]);
   const updatedUrl = new URL(window.location.href);
   tabOnClick(tabs[updatedUrl.searchParams.get(urlPathname[2])]);
+  setActiveTabInput(updatedUrl.searchParams.get(urlPathname[2]));
 }
