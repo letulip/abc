10a0d607a198c5262ead4164e8738f84124c1e84
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2020-04-24 14:56:27 +0300

    feat: add checkPrevActiveTab functional

--- src/blocks/tabs/tabs2.js
+++ src/blocks/tabs/tabs2.js
@@ -1,4 +1,14 @@
 const tabs = document.querySelectorAll(`.tabs__tab--second`);
+const url = new URL(window.location.href);
+const urlPathname = url.pathname.split('/');
+
+let urlPageParam = ``;
+
+const assignUrl = (tab, index) => {
+  tabOnClick(tab, index);
+  url.searchParams.set(urlPathname[2], index);
+  window.location.assign(url.href);
+};
 
 const removePrevActive = () => {
   tabs.forEach((tab) => {
@@ -11,12 +21,34 @@ const removePrevActive = () => {
 const tabOnClick = (tab) => {
   removePrevActive();
   tab.classList.add(`tabs__tab--second-active`);
+  
 };
 
-tabs.forEach((tab) => {
+tabs.forEach((tab, index) => {
   tab.addEventListener(`click`, (evt) => {
-    tabOnClick(evt.target);
+    // assignUrl(evt.target, index);
+    urlPageParam = `${urlPathname[2]}=${index}`
+    generateUrlParam(urlPageParam);
+    tabOnClick(evt.target, index);
   });
 });
 
-tabOnClick(tabs[0]);
+const generateUrlParam = (urlParam) => {
+  const stateObj = {
+    param: `0`,
+  }
+  history.pushState(stateObj, `ignored title`, `?${urlParam}`);
+};
+
+if (document.referrer.split('?').length === 1 ||
+    !document.referrer.split('/').includes(urlPathname[2]) ||
+    !document.referrer.split('/').includes(urlPathname[3])) {
+  urlPageParam = `${urlPathname[2]}=0`
+  generateUrlParam(urlPageParam);
+  tabOnClick(tabs[0]);
+} else {
+  const params = document.referrer.split('?');
+  generateUrlParam(params[1]);
+  const updatedUrl = new URL(window.location.href);
+  tabOnClick(tabs[updatedUrl.searchParams.get(urlPathname[2])]);
+}
